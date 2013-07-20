d012516df9ee14cb77ee6dc2095726df73809a46
Modric <Modric@Modric-PC>
2013-07-20 12:43:41 -0300

    Adicionada classe FormaPagamento

---
+++ BeautySystemTCC/src/br/com/tcc/DomainModel/FormaPagemento.java
@@ -0,0 +1,65 @@
+package br.com.tcc.DomainModel;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Thaisa
+ */
+public class FormaPagemento {
+    private int idFormaPagamento;
+    private String nome;
+
+    public FormaPagemento() {
+        this.idFormaPagamento = 0;
+        this.nome = "";
+    }
+
+    public int getIdFormaPagamento() {
+        return idFormaPagamento;
+    }
+
+    public void setIdFormaPagamento(int idFormaPagamento) {
+        this.idFormaPagamento = idFormaPagamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + this.idFormaPagamento;
+        hash = 37 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FormaPagemento other = (FormaPagemento) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+    
+    
+    
+}
